efa9ce891752c9305fcb85305d4bda47ef01a5f0
tcsimm <widet77@icloud.com>
2024-07-06 18:38:48 -0400

    pancakes

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -3,21 +3,20 @@ export default function Lab1() {
     <div id="wd-lab1">
       <h2>Lab 1</h2>
       <h3>HTML Examples</h3>
-      <div id="wd-h-tag">
-        <h4>Heading Tags</h4>
-        <p>Text documents are often broken up into several sections and subsections. Each section is usually prefaced with a short title or heading that attempts to summarize the topic of the section it precedes. For instance, this paragraph is preceded by the heading Heading Tags. The font of the section headings are usually larger and bolder than their subsection headings. This document uses headings to introduce topics such as HTML Documents, HTML Tags, Heading Tags, etc. HTML heading tags can be used to format plain text so that it renders in a browser as large headings. There are 6 heading tags for different sizes: h1, h2, h3, h4, h5, and h6. Tag h1 is the largest heading and h6 is the smallest heading.</p>
-      </div>
-      <div id="wd-p-tag">
-        <h4>Paragraph Tag</h4>
-        <p id="wd-p-1">
-          This is the first paragraph. The paragraph tag is used to format vertical gaps between long pieces of text like this one.
-        </p>
-        <p id="wd-p-2">
-          This is the second paragraph. Even though there is a deliberate white gap between the paragraph above and this paragraph, by default browsers render them as one contiguous piece of text as shown here on the right.
-        </p>
-        <p id="wd-p-3">
-          This is the third paragraph. Wrap each paragraph with the paragraph tag to tell browsers to render the gaps.
-        </p>
+      <div id="wd-h-tag"> ... </div>
+      <div id="wd-p-tag"> ... </div>
+      <div id="wd-lists">
+        <h4>List Tags</h4>
+        <h5>Ordered List Tag</h5>
+        How to make pancakes:
+        1. Mix dry ingredients.
+        2. Add wet ingredients.
+        3. Stir to combine.
+        4. Heat a skillet or griddle.
+        5. Pour batter onto the skillet.
+        6. Cook until bubbly on top.
+        7. Flip and cook the other side.
+        8. Serve and enjoy!
       </div>
     </div>
   );
