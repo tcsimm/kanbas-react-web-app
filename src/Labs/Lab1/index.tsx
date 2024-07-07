ff1c224d578b6ffeebe08dac73a401ae0ff41593
tcsimm <widet77@icloud.com>
2024-07-06 20:33:28 -0400

    format

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -144,7 +144,7 @@ export default function Lab1() {
             </tr>
           </tfoot>
         </table>
-      </div>
+        </div>
       <div id="wd-images">
         <h4>Image Tag</h4>
         <p>Loading an image from the internet:</p>
@@ -158,22 +158,22 @@ export default function Lab1() {
         <img id="wd-teslabot" src={teslabotImg} height="200px" alt="Tesla Bot" />
       </div>
       <div id="wd-forms">
-  <h4>Form Elements</h4>
-  <form id="wd-text-fields">
-    <h5>Text Fields</h5>
-    <label htmlFor="wd-text-fields-username">Username:</label>
-    <input id="wd-text-fields-username" placeholder="jdoe" /> <br />
-    <label htmlFor="wd-text-fields-password">Password:</label>
-    <input type="password" id="wd-text-fields-password" value="123@#$asd" />
-    <br />
-    <label htmlFor="wd-text-fields-first-name">First name:</label>
-    <input type="text" id="wd-text-fields-first-name" title="John" /> <br />
-    <label htmlFor="wd-text-fields-last-name">Last name:</label>
-    <input type="text" id="wd-text-fields-last-name" placeholder="Doe"
-      value="Wonderland" title="The last name" />
-    {/* copy rest of form elements here  */}
-  </form>
-</div>
+        <h4>Form Elements</h4>
+        <form id="wd-text-fields">
+          <h5>Text Fields</h5>
+          <label htmlFor="wd-text-fields-username">Username:</label>
+          <input id="wd-text-fields-username" placeholder="jdoe" /> <br />
+          <label htmlFor="wd-text-fields-password">Password:</label>
+          <input type="password" id="wd-text-fields-password" value="123@#$asd" />
+          <br />
+          <label htmlFor="wd-text-fields-first-name">First name:</label>
+          <input type="text" id="wd-text-fields-first-name" title="John" /> <br />
+          <label htmlFor="wd-text-fields-last-name">Last name:</label>
+          <input type="text" id="wd-text-fields-last-name" placeholder="Doe"
+            value="Wonderland" title="The last name" />
+          {/* copy rest of form elements here  */}
+        </form>
+      </div>
     </div>
   );
 }
