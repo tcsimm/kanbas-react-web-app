c4fda440b3b336c3fc10f28b6d90812e5d479132
tcsimm <widet77@icloud.com>
2024-07-07 08:19:40 -0400

    editor more

--- src/Kanbas/Courses/Assignments/editor.tsx
+++ src/Kanbas/Courses/Assignments/editor.tsx
@@ -4,7 +4,7 @@ export default function AssignmentEditor() {
         <label htmlFor="wd-name">Assignment Name</label>
         <input id="wd-name" value="A1 - ENV + HTML" /><br /><br />
         <textarea id="wd-description">
-          The assignment is available online Submit a link to the landing page of
+          The assignment is available online Submit a link to the landing page of your Web application running on Netlify. The landing page should include the following: Your full name and section Links to each of the lab assignments Link to the Kanbas application Links to all relevant source code repositories The Kanbas application should include a link to navigate back to the landing page.
         </textarea>
         <br />
         <table>
@@ -16,9 +16,84 @@ export default function AssignmentEditor() {
               <input id="wd-points" value={100} />
             </td>
           </tr>
-          {/* Complete on your own */}
+          <tr>
+            <td align="right" valign="top">
+              <label htmlFor="wd-assignment-group">Assignment Group</label>
+            </td>
+            <td>
+              <select id="wd-assignment-group">
+                <option value="assignments">ASSIGNMENTS</option>
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td align="right" valign="top">
+              <label htmlFor="wd-display-grade">Display Grade as</label>
+            </td>
+            <td>
+              <select id="wd-display-grade">
+                <option value="percentage">Percentage</option>
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td align="right" valign="top">
+              <label htmlFor="wd-submission-type">Submission Type</label>
+            </td>
+            <td>
+              <select id="wd-submission-type">
+                <option value="online">Online</option>
+              </select>
+              <div>
+                <input type="checkbox" id="wd-text-entry" />
+                <label htmlFor="wd-text-entry">Text Entry</label><br />
+                <input type="checkbox" id="wd-website-url" />
+                <label htmlFor="wd-website-url">Website URL</label><br />
+                <input type="checkbox" id="wd-media-recordings" />
+                <label htmlFor="wd-media-recordings">Media Recordings</label><br />
+                <input type="checkbox" id="wd-student-annotation" />
+                <label htmlFor="wd-student-annotation">Student Annotation</label><br />
+                <input type="checkbox" id="wd-file-uploads" />
+                <label htmlFor="wd-file-uploads">File Uploads</label>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td align="right" valign="top">
+              <label htmlFor="wd-assign-to">Assign to</label>
+            </td>
+            <td>
+              <input id="wd-assign-to" value="Everyone" />
+            </td>
+          </tr>
+          <tr>
+            <td align="right" valign="top">
+              <label htmlFor="wd-due">Due</label>
+            </td>
+            <td>
+              <input type="date" id="wd-due" value="2024-05-13" />
+            </td>
+          </tr>
+          <tr>
+            <td align="right" valign="top">
+              <label htmlFor="wd-available-from">Available from</label>
+            </td>
+            <td>
+              <input type="date" id="wd-available-from" value="2024-05-06" />
+            </td>
+          </tr>
+          <tr>
+            <td align="right" valign="top">
+              <label htmlFor="wd-available-until">Until</label>
+            </td>
+            <td>
+              <input type="date" id="wd-available-until" value="2024-05-20" />
+            </td>
+          </tr>
         </table>
+        <button>Cancel</button>
+        <button>Save</button>
       </div>
-  );}
-  
+    );
+  }
   
