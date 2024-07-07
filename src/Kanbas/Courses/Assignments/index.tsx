3bf0e8c36f2abd3ccbc078373a73a7bc0a12e4e1
tcsimm <widet77@icloud.com>
2024-07-07 07:55:31 -0400

    more Assignments

--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -1,8 +1,7 @@
 export default function Assignments() {
     return (
       <div id="wd-assignments">
-        <input id="wd-search-assignment"
-               placeholder="Search for Assignments" />
+        <input id="wd-search-assignment" placeholder="Search for Assignments" />
         <button id="wd-add-assignment-group">+ Group</button>
         <button id="wd-add-assignment">+ Assignment</button>
         <h3 id="wd-assignments-title">
@@ -10,16 +9,31 @@ export default function Assignments() {
         </h3>
         <ul id="wd-assignment-list">
           <li className="wd-assignment-list-item">
-            <a className="wd-assignment-link"
-              href="#/Kanbas/Courses/1234/Assignments/123">
+            <a className="wd-assignment-link" href="#/Kanbas/Courses/1234/Assignments/123">
               A1 - ENV + HTML
             </a>
+            <div>
+              Multiple Modules | <span className="wd-assignment-status">Not available until May 6 at 12:00am</span> | Due May 13 at 11:59pm | 100 pts
+            </div>
           </li>
           <li className="wd-assignment-list-item">
-            {/* Complete On Your Own */}
+            <a className="wd-assignment-link" href="#/Kanbas/Courses/1234/Assignments/124">
+              A2 - CSS + BOOTSTRAP
+            </a>
+            <div>
+              Multiple Modules | <span className="wd-assignment-status">Not available until May 13 at 12:00am</span> | Due May 20 at 11:59pm | 100 pts
+            </div>
+          </li>
+          <li className="wd-assignment-list-item">
+            <a className="wd-assignment-link" href="#/Kanbas/Courses/1234/Assignments/125">
+              A3 - JAVASCRIPT + REACT
+            </a>
+            <div>
+              Multiple Modules | <span className="wd-assignment-status">Not available until May 20 at 12:00am</span> | Due May 27 at 11:59pm | 100 pts
+            </div>
           </li>
         </ul>
       </div>
-  );}
-  
+    );
+  }
   
