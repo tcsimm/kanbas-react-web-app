0e499bd4660f7a8a7a3ca241daff82fd67fe97c8
tcsimm <widet77@icloud.com>
2024-07-07 07:18:31 -0400

    side

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,30 +1,39 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./Dashboard";
-import KanbasNavigation from "./Navigation";
-import { Routes, Route, Navigate } from "react-router";
 import Courses from "./Courses";
 
 export default function Kanbas() {
   return (
     <div id="wd-kanbas">
       <table>
-        <tbody>
-          <tr>
-            <td valign="top">
-              <KanbasNavigation />
-            </td>
-            <td valign="top">
-              <Routes>
-                <Route path="/" element={<Navigate to="Dashboard" />} />
-                <Route path="Account" element={<h1>Account</h1>} />
-                <Route path="Dashboard" element={<Dashboard />} />
-                <Route path="Courses/:id/*" element={<Courses />} />
-                <Route path="Calendar" element={<h1>Calendar</h1>} />
-                <Route path="Inbox" element={<h1>Inbox</h1>} />
-              </Routes>
-            </td>
-          </tr>
-        </tbody>
+        <tr>
+          <td valign="top">
+            <KanbasNavigation />
+          </td>
+          <td valign="top">
+            <Routes>
+              <Route path="/" element={<Navigate to="Dashboard" />} />
+              <Route path="Dashboard" element={<Dashboard />} />
+              <Route path="Courses/*" element={<Courses />} />
+            </Routes>
+          </td>
+        </tr>
       </table>
     </div>
+  );
+}
+
+function KanbasNavigation() {
+  return (
+    <ul id="wd-kanbas-navigation">
+      <li><a id="wd-neu-link" target="_blank" rel="noreferrer" href="https://www.northeastern.edu/">Northeastern</a></li>
+      <li><a id="wd-account-link" href="#/Kanbas/Account">Account</a></li>
+      <li><a id="wd-dashboard-link" href="#/Kanbas/Dashboard">Dashboard</a></li>
+      <li><a id="wd-course-link" href="#/Kanbas/Courses">Courses</a></li>
+      <li><a id="wd-calendar-link" href="#/Kanbas/Calendar">Calendar</a></li>
+      <li><a id="wd-inbox-link" href="#/Kanbas/Inbox">Inbox</a></li>
+      <li><a id="wd-labs-link" href="#/Labs">Labs</a></li>
+    </ul>
   );
 }
