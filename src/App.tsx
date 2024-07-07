fb8e5cd7e6dd4a2c35e849633bea419aad15e2f1
tcsimm <widet77@icloud.com>
2024-07-06 21:44:59 -0400

    tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import React from "react";
 import Labs from "./Labs";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 function App() {
   return (
-    <div>
-      <Labs />
-    </div>
+    <HashRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="Labs" />} />
+          <Route path="/Labs/*" element={<Labs />} />
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
 export default App;
+
