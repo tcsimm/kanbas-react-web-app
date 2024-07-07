ea5822703dcb907d9eecd60d303125b2e0e99296
tcsimm <widet77@icloud.com>
2024-07-06 21:25:24 -0400

    dropdowns

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -216,6 +216,27 @@ export default function Lab1() {
 
 <input type="checkbox" name="check-genre" id="wd-chkbox-fantasy"/>
 <label htmlFor="wd-chkbox-fantasy">Fantasy</label>
+<h4 id="wd-dropdowns">Dropdowns</h4>
+
+<h5>Select one</h5>
+<label htmlFor="wd-select-one-genre"> Favorite movie genre: </label><br/>
+<select id="wd-select-one-genre">
+   <option value="COMEDY">Comedy</option>
+   <option value="DRAMA">Drama</option>
+   <option selected value="SCIFI">
+       Science Fiction</option>
+   <option value="FANTASY">Fantasy</option>
+</select>
+
+<h5>Select many</h5>
+<label htmlFor="wd-select-many-genre"> Favorite movie genres: </label><br/>
+<select id="wd-select-many-genre" multiple>
+   <option selected value="COMEDY">Comedy</option>
+   <option value="DRAMA">Drama</option>
+   <option selected value="SCIFI">
+       Science Fiction</option>
+   <option value="FANTASY">Fantasy</option>
+</select>
     </div>
   );
 }
