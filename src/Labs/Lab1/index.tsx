b80c8d4642d996bdad1fee0a9fcd31c50e7e9799
tcsimm <widet77@icloud.com>
2024-07-06 21:21:09 -0400

    buttons

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -190,6 +190,18 @@ export default function Lab1() {
         <h5>File upload</h5>
         <input id="wd-upload" type="file" />
       </div>
+      <div id="wd-radio-buttons">
+        <h5>Radio buttons</h5>
+        <label>Favorite movie genre:</label><br />
+        <input type="radio" name="radio-genre" id="wd-radio-comedy" />
+        <label htmlFor="wd-radio-comedy">Comedy</label><br />
+        <input type="radio" name="radio-genre" id="wd-radio-drama" />
+        <label htmlFor="wd-radio-drama">Drama</label><br />
+        <input type="radio" name="radio-genre" id="wd-radio-scifi" />
+        <label htmlFor="wd-radio-scifi">Science Fiction</label><br />
+        <input type="radio" name="radio-genre" id="wd-radio-fantasy" />
+        <label htmlFor="wd-radio-fantasy">Fantasy</label>
+      </div>
     </div>
   );
 }
