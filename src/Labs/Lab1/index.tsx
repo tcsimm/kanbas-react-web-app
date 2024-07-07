01b76e31c97726f5696d7ac6b9f3ac46b5c81301
tcsimm <widet77@icloud.com>
2024-07-06 21:23:10 -0400

    checkboxes# Please enter the commit message for your changes. Lines starting

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -202,6 +202,20 @@ export default function Lab1() {
         <input type="radio" name="radio-genre" id="wd-radio-fantasy" />
         <label htmlFor="wd-radio-fantasy">Fantasy</label>
       </div>
+      <h5 id="wd-checkboxes">Checkboxes</h5>
+<label>Favorite movie genre:</label><br/>
+
+<input type="checkbox" name="check-genre" id="wd-chkbox-comedy"/>
+<label htmlFor="wd-chkbox-comedy">Comedy</label><br/>
+
+<input type="checkbox" name="check-genre" id="wd-chkbox-drama"/>
+<label htmlFor="wd-chkbox-drama">Drama</label><br/>
+
+<input type="checkbox" name="check-genre" id="wd-chkbox-scifi"/>
+<label htmlFor="wd-chkbox-scifi">Science Fiction</label><br/>
+
+<input type="checkbox" name="check-genre" id="wd-chkbox-fantasy"/>
+<label htmlFor="wd-chkbox-fantasy">Fantasy</label>
     </div>
   );
 }
