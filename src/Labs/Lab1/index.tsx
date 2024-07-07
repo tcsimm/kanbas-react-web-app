4da88ee4c01d6b2b5682118d7cdb08f6c6de3734
tcsimm <widet77@icloud.com>
2024-07-06 21:12:47 -0400

    fileupload

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -186,6 +186,10 @@ export default function Lab1() {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </textarea>
       </div>
+      <div id="wd-file-upload">
+        <h5>File upload</h5>
+        <input id="wd-upload" type="file" />
+      </div>
     </div>
   );
 }
