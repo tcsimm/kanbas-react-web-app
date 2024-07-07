e580609e7929b889eac68711f06b2ba47f72b83a
tcsimm <widet77@icloud.com>
2024-07-06 22:23:25 -0400

    kanbas

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,7 +1,8 @@
+import Dashboard from "./Dashboard";
 export default function Kanbas() {
   return (
     <div id="wd-kanbas">
-      <h1>Kanbas</h1>
+      <Dashboard />
     </div>
-);}
-
+  );
+}
