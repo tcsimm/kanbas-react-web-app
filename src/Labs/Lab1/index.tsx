97051bb478ab2f1980126deedd4f3264db69bc6a
tcsimm <widet77@icloud.com>
2024-07-06 20:30:15 -0400

    form

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -157,6 +157,23 @@ export default function Lab1() {
         <p>Loading a local image:</p>
         <img id="wd-teslabot" src={teslabotImg} height="200px" alt="Tesla Bot" />
       </div>
+      <div id="wd-forms">
+  <h4>Form Elements</h4>
+  <form id="wd-text-fields">
+    <h5>Text Fields</h5>
+    <label htmlFor="wd-text-fields-username">Username:</label>
+    <input id="wd-text-fields-username" placeholder="jdoe" /> <br />
+    <label htmlFor="wd-text-fields-password">Password:</label>
+    <input type="password" id="wd-text-fields-password" value="123@#$asd" />
+    <br />
+    <label htmlFor="wd-text-fields-first-name">First name:</label>
+    <input type="text" id="wd-text-fields-first-name" title="John" /> <br />
+    <label htmlFor="wd-text-fields-last-name">Last name:</label>
+    <input type="text" id="wd-text-fields-last-name" placeholder="Doe"
+      value="Wonderland" title="The last name" />
+    {/* copy rest of form elements here  */}
+  </form>
+</div>
     </div>
   );
 }
