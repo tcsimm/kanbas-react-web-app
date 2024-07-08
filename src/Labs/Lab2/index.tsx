6f9bdbaca3eef25cd3af7d61f7ece5e3b929ff2f
tcsimm <widet77@icloud.com>
2024-07-08 12:07:33 -0400

    more margins

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -1,10 +1,7 @@
-// src/Labs/Lab2/index.tsx
-
-import React from 'react';
-import './index.css';
-import Borders from './Borders';
-import Padding from './Padding';
-import Margins from './Margins'; // Import the Margins component
+import "./index.css";
+import Borders from "./Borders";
+import Margins from "./Margins";
+import Padding from "./Padding";
 
 export default function Lab2() {
   return (
@@ -58,9 +55,39 @@ export default function Lab2() {
           </div>
         </div>
       </div>
-      <Borders />
-      <Padding />
-      <Margins />
+      <div id="wd-css-borders">
+        <h2>Borders</h2>
+        <p className="wd-border-fat wd-border-red wd-border-solid">
+          Solid fat red border
+        </p>
+        <p className="wd-border-thin wd-border-blue wd-border-dashed">
+          Dashed thin blue border
+        </p>
+      </div>
+      <div id="wd-css-padding">
+        <h2>Padding</h2>
+        <div className="wd-padded-top-left wd-border-fat wd-border-red wd-border-solid wd-bg-color-yellow">
+          Padded top left
+        </div>
+        <div className="wd-padded-bottom-right wd-border-fat wd-border-blue wd-border-solid wd-bg-color-yellow">
+          Padded bottom right
+        </div>
+        <div className="wd-padding-fat wd-border-fat wd-border-yellow wd-border-solid wd-bg-color-blue wd-fg-color-white">
+          Padded all around
+        </div>
+      </div>
+      <div id="wd-css-margins">
+        <h2>Margins</h2>
+        <div className="wd-margin-bottom wd-padded-top-left wd-border-fat wd-border-red wd-border-solid wd-bg-color-yellow">
+          Margin bottom
+        </div>
+        <div className="wd-margin-right-left wd-padded-bottom-right wd-border-fat wd-border-blue wd-border-solid wd-bg-color-yellow">
+          Margin left right
+        </div>
+        <div className="wd-margin-all-around wd-padding-fat wd-border-fat wd-border-yellow wd-border-solid wd-bg-color-blue wd-fg-color-white">
+          Margin all around
+        </div>
+      </div>
     </div>
   );
 }
