ea97965eda54a2447dda12e49ac3eb52bf0d14f7
tcsimm <widet77@icloud.com>
2024-07-08 19:17:10 -0400

    more

--- src/Kanbas/Navigation/index.tsx
+++ src/Kanbas/Navigation/index.tsx
@@ -1,43 +1,29 @@
-import React from 'react';
-import { AiOutlineDashboard } from "react-icons/ai";
-import { IoCalendarOutline } from "react-icons/io5";
-import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
-import { FaInbox, FaRegCircle } from "react-icons/fa";
-
 export default function KanbasNavigation() {
   return (
-    <div id="wd-kanbas-navigation" className="list-group position-fixed top-0 bottom-0 d-none d-md-block z-2 bg-black" style={{ width: 120 }}>
-      <a id="wd-neu-link" target="_blank" href="https://www.northeastern.edu/" className="list-group-item bg-black border-0">
-        <img src="/images/NEU.png" width="75px" alt="Northeastern University" />
-      </a>
-      <a id="wd-account-link" href="#/Kanbas/Account" className="list-group-item text-white bg-black text-center border-0">
-        <FaRegCircle className="fs-1 text text-white" /><br />
-        Account
-      </a>
-      <a id="wd-dashboard-link" href="#/Kanbas/Dashboard" className="list-group-item text-center border-0 bg-white text-danger">
-        <AiOutlineDashboard className="fs-1 text-danger" /><br />
-        Dashboard
-      </a>
-      <a id="wd-course-link" href="#/Kanbas/Courses" className="list-group-item text-white bg-black text-center border-0">
-        <LiaBookSolid className="fs-1 text-danger" /><br />
-        Courses
-      </a>
-      <a id="wd-calendar-link" href="#/Kanbas/Calendar" className="list-group-item text-white bg-black text-center border-0">
-        <IoCalendarOutline className="fs-1 text-danger" /><br />
-        Calendar
-      </a>
-      <a id="wd-inbox-link" href="#/Kanbas/Inbox" className="list-group-item text-white bg-black text-center border-0">
-        <FaInbox className="fs-1 text-danger" /><br />
-        Inbox
-      </a>
-      <a id="wd-labs-link" href="#/Labs" className="list-group-item text-white bg-black text-center border-0">
-        <LiaCogSolid className="fs-1 text-danger" /><br />
-        Labs
-      </a>
-      <a id="wd-landing-page-link" href="#/Kanbas/LandingPage" className="list-group-item text-white bg-black text-center border-0">
-        <LiaCogSolid className="fs-1 text-danger" /><br />
-        Landing Page
-      </a>
-    </div>
+    <ul id="wd-kanbas-navigation">
+      <li>
+        <a id="wd-neu-link" target="_blank" rel="noreferrer" href="https://www.northeastern.edu/">
+          Northeastern
+        </a>
+      </li>
+      <li>
+        <a id="wd-account-link" href="#/Kanbas/Account">Account</a>
+      </li>
+      <li>
+        <a id="wd-dashboard-link" href="#/Kanbas/Dashboard">Dashboard</a>
+      </li>
+      <li>
+        <a id="wd-course-link" href="#/Kanbas/Courses">Courses</a>
+      </li>
+      <li>
+        <a id="wd-calendar-link" href="#/Kanbas/Calendar">Calendar</a>
+      </li>
+      <li>
+        <a id="wd-inbox-link" href="#/Kanbas/Inbox">Inbox</a>
+      </li>
+      <li>
+        <a id="wd-labs-link" href="#/Labs">Labs</a>
+      </li>
+    </ul>
   );
 }
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,27 +1,43 @@
-import React from 'react';
 import { Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Courses from "./Courses";
 import LandingPage from "../LandingPage";
-import KanbasNavigation from "./Navigation";
-import './index.css';
 
 export default function Kanbas() {
   return (
-    <div id="wd-kanbas" className="container-fluid">
-      <div className="row">
-        <div className="col-12 col-md-3 mb-3">
-          <KanbasNavigation />
-        </div>
-        <div className="col-12 col-md-9">
-          <Routes>
-            <Route path="/" element={<Navigate to="Dashboard" />} />
-            <Route path="Dashboard" element={<Dashboard />} />
-            <Route path="Courses/*" element={<Courses />} />
-            <Route path="LandingPage" element={<LandingPage />} />
-          </Routes>
-        </div>
-      </div>
+    <div id="wd-kanbas">
+      <table>
+        <tbody>
+          <tr>
+            <td valign="top">
+              <KanbasNavigation />
+            </td>
+            <td valign="top">
+              <Routes>
+                <Route path="/" element={<Navigate to="Dashboard" />} />
+                <Route path="Dashboard" element={<Dashboard />} />
+                <Route path="Courses/*" element={<Courses />} />
+                <Route path="LandingPage" element={<LandingPage />} />
+              </Routes>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
+  );
+}
+
+function KanbasNavigation() {
+  return (
+    <ul id="wd-kanbas-navigation">
+      <li><a id="wd-neu-link" target="_blank" rel="noreferrer" href="https://www.northeastern.edu/">Northeastern</a></li>
+      <li><a id="wd-account-link" href="#/Kanbas/Account">Account</a></li>
+      <li><a id="wd-dashboard-link" href="#/Kanbas/Dashboard">Dashboard</a></li>
+      <li><a id="wd-course-link" href="#/Kanbas/Courses">Courses</a></li>
+      <li><a id="wd-calendar-link" href="#/Kanbas/Calendar">Calendar</a></li>
+      <li><a id="wd-inbox-link" href="#/Kanbas/Inbox">Inbox</a></li>
+      <li><a id="wd-labs-link" href="#/Labs">Labs</a></li>
+      <li><a id="wd-landing-page-link" href="#/Kanbas/LandingPage">Landing Page</a></li> {/* Add link to Landing Page */}
+    </ul>
   );
 }
