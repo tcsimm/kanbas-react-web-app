f1314d2f32396f58dd70766c49fd2c267314d853
tcsimm <widet77@icloud.com>
2024-07-08 11:34:35 -0400

    yellow

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -23,6 +23,15 @@ export default function Lab2() {
           Here's another paragraph using a different ID and a different look and feel
         </p>
       </div>
+      <div id="wd-css-class-selectors">
+        <h3>Class selectors</h3>
+        <p className="wd-class-selector">
+          Instead of using IDs to refer to elements, you can use an element's CLASS attribute
+        </p>
+        <h4 className="wd-class-selector">
+          This heading has same style as paragraph above
+        </h4>
+      </div>
     </div>
   );
 }
