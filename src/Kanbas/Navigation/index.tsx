3c08453467cd56e3d9516326f552d5513466ccfc
tcsimm <widet77@icloud.com>
2024-07-08 19:46:04 -0400

    move#

--- src/Kanbas/Navigation/index.tsx
+++ src/Kanbas/Navigation/index.tsx
@@ -2,7 +2,7 @@ import { AiOutlineDashboard } from "react-icons/ai";
 import { IoCalendarOutline } from "react-icons/io5";
 import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
 import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
-import "./styles.css"; // Make sure to import the styles.css file
+import "./styles.css"; 
 
 export default function KanbasNavigation() {
   return (
