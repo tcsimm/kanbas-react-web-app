caa1a37d83af2b127546e59e63c5ae5ea5cf423b
tcsimm <widet77@icloud.com>
2024-07-08 14:30:27 -0400

    toc

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,12 +1,39 @@
+import { useLocation } from "react-router-dom";
+
 export default function TOC() {
+  const { pathname } = useLocation();
   return (
-    <ul>
-      <li><a id="wd-a"  href="#/Labs">Labs</a></li>
-      <li><a id="wd-a1" href="#/Labs/Lab1">Lab 1</a></li>
-      <li><a id="wd-a2" href="#/Labs/Lab2">Lab 2</a></li>
-      <li><a id="wd-a3" href="#/Labs/Lab3">Lab 3</a></li>
-      <li><a id="wd-k"  href="#/Kanbas">Kanbas</a></li>
-      <li><a id="wd-github" href="https://github.com/tcsimm/kanbas-react-web-app" target="_blank" rel="noopener noreferrer">GitHub Repository</a></li>
+    <ul className="nav nav-pills">
+      <li className="nav-item">
+        <a id="wd-a" href="#/Labs" className={`nav-link ${pathname === "/Labs" ? "active" : ""}`}>
+          Labs
+        </a>
+      </li>
+      <li className="nav-item">
+        <a id="wd-a1" href="#/Labs/Lab1" className={`nav-link ${pathname.includes("Lab1") ? "active" : ""}`}>
+          Lab 1
+        </a>
+      </li>
+      <li className="nav-item">
+        <a id="wd-a2" href="#/Labs/Lab2" className={`nav-link ${pathname.includes("Lab2") ? "active" : ""}`}>
+          Lab 2
+        </a>
+      </li>
+      <li className="nav-item">
+        <a id="wd-a3" href="#/Labs/Lab3" className={`nav-link ${pathname.includes("Lab3") ? "active" : ""}`}>
+          Lab 3
+        </a>
+      </li>
+      <li className="nav-item">
+        <a id="wd-k" href="#/Kanbas" className={`nav-link ${pathname === "/Kanbas" ? "active" : ""}`}>
+          Kanbas
+        </a>
+      </li>
+      <li className="nav-item">
+        <a id="wd-github" href="https://github.com/tcsimm/kanbas-react-web-app" target="_blank" rel="noopener noreferrer" className="nav-link">
+          GitHub Repository
+        </a>
+      </li>
     </ul>
   );
 }
