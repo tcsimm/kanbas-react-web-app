f7a844dba09e50d083c4ac96a1ea5bac9fee3197
tcsimm <widet77@icloud.com>
2024-07-08 20:49:26 -0400

    more

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,37 +1,36 @@
+import React from 'react';
+import { Navigate, Route, Routes } from "react-router";
 import CoursesNavigation from "./Navigation";
 import Modules from "./Modules";
 import Home from "./Home";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/editor";
-import { Navigate, Route, Routes } from "react-router";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
 
 export default function Courses() {
   return (
-    <div id="wd-courses">
-      <h2>Course 1234</h2>
+    <div id="wd-courses" className="container-fluid">
+      <h2 className="text-danger my-3">Course 1234</h2>
       <hr />
-      <table>
-        <tbody>
-          <tr>
-            <td valign="top">
-              <CoursesNavigation />
-            </td>
-            <td valign="top">
-              <Routes>
-                <Route path="/" element={<Navigate to="Home" />} />
-                <Route path="Home" element={<Home />} />
-                <Route path="Modules" element={<Modules />} />
-                <Route path="Piazza" element={<h3>Piazza</h3>} />
-                <Route path="Zoom" element={<h4>Zoom</h4>} />
-                <Route path="Assignments" element={<Assignments />} />
-                <Route path="Assignments/:id" element={<AssignmentEditor />} />
-                <Route path="Quizzes" element={<h6>Quizzes</h6>} />
-                <Route path="Grades" element={<h6>Grades</h6>} />
-              </Routes>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="row">
+        <div className="col-md-3 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2">
+          <CoursesNavigation />
+        </div>
+        <div className="col-md-9 offset-md-3 p-3 wd-main-content-offset">
+          <Routes>
+            <Route path="/" element={<Navigate to="Home" />} />
+            <Route path="Home" element={<Home />} />
+            <Route path="Modules" element={<Modules />} />
+            <Route path="Piazza" element={<h3>Piazza</h3>} />
+            <Route path="Zoom" element={<h4>Zoom</h4>} />
+            <Route path="Assignments" element={<Assignments />} />
+            <Route path="Assignments/:id" element={<AssignmentEditor />} />
+            <Route path="Quizzes" element={<h6>Quizzes</h6>} />
+            <Route path="Grades" element={<h6>Grades</h6>} />
+          </Routes>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Kanbas/Navigation/index.tsx
+++ src/Kanbas/Navigation/index.tsx
@@ -2,7 +2,7 @@ import { AiOutlineDashboard } from "react-icons/ai";
 import { IoCalendarOutline } from "react-icons/io5";
 import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
 import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
-import "../styles.css"; // Ensure the correct path if this file is in Navigation subdirectory
+import "../styles.css"; 
 
 export default function KanbasNavigation() {
   return (
