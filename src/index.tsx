efb4d8b400a40d30be6728c51d5c5e88f4cb3253
tcsimm <widet77@icloud.com>
2024-07-10 08:11:34 -0400

    4.7 5

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js"; 
-import { HashRouter } from "react-router-dom";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>
 );
-
 reportWebVitals();
+
