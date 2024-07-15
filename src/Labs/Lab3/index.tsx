5640041eb1503532357bbe7156ff3d0766d3087c
tcsimm <widet77@icloud.com>
2024-07-15 11:28:00 -0400

    BooleanVariables

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,10 +1,14 @@
 import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+import BooleanVariables from "./BooleanVariables";
 
 export default function Lab3() {
   return (
     <div id="wd-lab3" className="container-fluid">
       <h3>Lab 3</h3>
-      <VariablesAndConstants/>
+      <VariablesAndConstants />
+      <VariableTypes />
+      <BooleanVariables />
     </div>
   );
 }
