3ce63559703d759e5b483262b3426b43e11b2fc5
tcsimm <widet77@icloud.com>
2024-07-15 14:44:44 -0400

    more

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -37,7 +37,7 @@ export default function Lab3() {
   console.log('Hello World!');
   return (
     <div id="wd-lab3">
-      <h3>JavaScript</h3>
+      <h3>Lab 3</h3>
       <VariablesAndConstants />
       <VariableTypes />
       <BooleanVariables />
