e8f97655878bc0a5708601fd94c4748162734d2f
tcsimm <widet77@icloud.com>
2024-07-15 11:06:58 -0400

    first commit

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,9 +1,7 @@
 export default function Lab3() {
-    return (
-      <div>
-        <h2>Lab 3</h2>
-      </div>
-    );
-  }
-  
-  
+  return (
+    <div id="wd-lab3" className="container-fluid">
+      <h3>Lab 3</h3>
+    </div>
+  );
+}
