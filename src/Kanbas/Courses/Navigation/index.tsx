dce428da81483d88401e15a6c86eae545c0ae235
tcsimm <widet77@icloud.com>
2024-07-15 18:54:58 -0400

    black line

--- src/Kanbas/Courses/Navigation/index.tsx
+++ src/Kanbas/Courses/Navigation/index.tsx
@@ -1,16 +1,33 @@
-import { Link } from "react-router-dom";
-import "./index.css";
+import { NavLink } from "react-router-dom";
+import { useLocation } from "react-router";
+import "../styles.css";
 
 export default function CoursesNavigation() {
+  const { pathname } = useLocation();
+
+  const links = [
+    { label: "Home", path: "Home" },
+    { label: "Modules", path: "Modules" },
+    { label: "Piazza", path: "Piazza" },
+    { label: "Zoom", path: "Zoom" },
+    { label: "Assignments", path: "Assignments" },
+    { label: "Quizzes", path: "Quizzes" },
+    { label: "Grades", path: "Grades" },
+  ];
+
   return (
-    <div id="wd-courses-navigation" className="list-group fs-5 rounded-0">
-      <Link to="Home" className="list-group-item active border border-0"> Home </Link>
-      <Link to="Modules" className="list-group-item text-danger border border-0"> Modules </Link>
-      <Link to="Piazza" className="list-group-item text-danger border border-0"> Piazza </Link>
-      <Link to="Zoom" className="list-group-item text-danger border border-0"> Zoom </Link>
-      <Link to="Assignments" className="list-group-item text-danger border border-0"> Assignments </Link>
-      <Link to="Quizzes" className="list-group-item text-danger border border-0"> Quizzes </Link>
-      <Link to="Grades" className="list-group-item text-danger border border-0"> Grades </Link>
+    <div className="list-group">
+      {links.map((link) => (
+        <NavLink
+          key={link.path}
+          to={link.path}
+          className={({ isActive }) =>
+            isActive ? "list-group-item active-link" : "list-group-item"
+          }
+        >
+          {link.label}
+        </NavLink>
+      ))}
     </div>
   );
 }
