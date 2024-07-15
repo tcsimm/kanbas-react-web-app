759e93c98337fac6797af1c9a30c62358823150d
tcsimm <widet77@icloud.com>
2024-07-15 11:14:13 -0400

    VariablesAndConstants

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,7 +1,10 @@
+import VariablesAndConstants from "./VariablesAndConstants";
+
 export default function Lab3() {
   return (
     <div id="wd-lab3" className="container-fluid">
       <h3>Lab 3</h3>
+      <VariablesAndConstants/>
     </div>
   );
 }
