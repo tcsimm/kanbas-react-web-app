db56dde46fff7eed9ced7da4cd352d4c4422cdd0
tcsimm <widet77@icloud.com>
2024-07-15 14:13:24 -0400

    colors

--- src/Labs/Lab3/Classes.tsx
+++ src/Labs/Lab3/Classes.tsx
@@ -1,8 +1,11 @@
 import './Classes.css';
 
 export default function Classes() {
+  const color = 'blue';
+  const dangerous = true;
+
   return (
-    <div>
+    <div id="wd-classes">
       <h2>Classes</h2>
       <div className="wd-bg-yellow wd-fg-black wd-padding-10px">
         Yellow background
@@ -12,6 +15,12 @@ export default function Classes() {
       </div>
       <div className="wd-bg-red wd-fg-black wd-padding-10px">
         Red background
+      </div>
+      <div className={`wd-bg-${color} wd-fg-black wd-padding-10px`}>
+        Dynamic Blue background
+      </div>
+      <div className={`${dangerous ? 'wd-bg-red' : 'wd-bg-green'} wd-fg-black wd-padding-10px`}>
+        Dangerous background
       </div>
       <hr />
     </div>
