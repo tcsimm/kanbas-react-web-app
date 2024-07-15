79d52c5f56eb1bce6cdcdf35c3ce6bd81a054b3d
tcsimm <widet77@icloud.com>
2024-07-15 18:01:52 -0400

    jpg 6

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -1,14 +1,29 @@
 import { Link } from "react-router-dom";
 import * as db from "../Database";
 import '../styles.css'; 
+
+// Import the images
 import rocketPropulsionImg from '../../images/rocket_propulsion.jpg';
+import aerodynamicsImg from '../../images/aerodynamics.jpg';
+import spacecraftDesignImg from '../../images/spacecraft_design.jpg';
+import organicChemistryImg from '../../images/organic_chemistry.jpg';
+import inorganicChemistryImg from '../../images/inorganic_chemistry.jpg';
+import physicalChemistryImg from '../../images/physical_chemistry.jpg';
+import defaultImg from '../../images/default.jpg'; // Default image
+
+// Map the image filenames to the imported images
+const imageMap: { [key: string]: string } = {
+  "rocket_propulsion.jpg": rocketPropulsionImg,
+  "aerodynamics.jpg": aerodynamicsImg,
+  "spacecraft_design.jpg": spacecraftDesignImg,
+  "organic_chemistry.jpg": organicChemistryImg,
+  "inorganic_chemistry.jpg": inorganicChemistryImg,
+  "physical_chemistry.jpg": physicalChemistryImg,
+  // Add other mappings similarly
+};
 
 export default function Dashboard() {
   const courses = db.courses;
-
-  const imageMap: { [key: string]: string } = {
-    "rocket_propulsion.jpg": rocketPropulsionImg,
-  };
 
   return (
     <div id="wd-dashboard" className="container-fluid">
@@ -21,7 +36,11 @@ export default function Dashboard() {
           <div className="col" key={course._id}>
             <div className="card">
               <Link className="wd-dashboard-course-link text-decoration-none text-dark" to={`/Kanbas/Courses/${course._id}/Home`}>
-                <img src={course.image} className="card-img-top" alt={`${course.name} Course`} />
+                <img 
+                  src={course.image && imageMap[course.image] ? imageMap[course.image] : defaultImg} 
+                  className="card-img-top" 
+                  alt={`${course.name} Course`} 
+                />
                 <div className="card-body">
                   <h5 className="wd-dashboard-course-title card-title">{course.name}</h5>
                   <p className="card-text">{course.description}</p>
