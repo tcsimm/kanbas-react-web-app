e487c4f344cfb6a8c5993f1cc452bcf85de32e3f
tcsimm <widet77@icloud.com>
2024-07-16 12:03:31 -0400

    fixing

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./Dashboard";
+import KanbasNavigation from "./Navigation";
+import { Routes, Route, Navigate } from "react-router";
 import Courses from "./Courses";
-import LandingPage from "../LandingPage";
 
 export default function Kanbas() {
   return (
@@ -18,27 +18,14 @@ export default function Kanbas() {
                 <Route path="/" element={<Navigate to="Dashboard" />} />
                 <Route path="Dashboard" element={<Dashboard />} />
                 <Route path="Courses/*" element={<Courses />} />
-                <Route path="LandingPage" element={<LandingPage />} />
+                <Route path="Account" element={<h1>Account</h1>} />
+                <Route path="Calendar" element={<h1>Calendar</h1>} />
+                <Route path="Inbox" element={<h1>Inbox</h1>} />
               </Routes>
             </td>
           </tr>
         </tbody>
       </table>
     </div>
-  );
-}
-
-function KanbasNavigation() {
-  return (
-    <ul id="wd-kanbas-navigation">
-      <li><a id="wd-neu-link" target="_blank" rel="noreferrer" href="https://www.northeastern.edu/">Northeastern</a></li>
-      <li><a id="wd-account-link" href="#/Kanbas/Account">Account</a></li>
-      <li><a id="wd-dashboard-link" href="#/Kanbas/Dashboard">Dashboard</a></li>
-      <li><a id="wd-course-link" href="#/Kanbas/Courses">Courses</a></li>
-      <li><a id="wd-calendar-link" href="#/Kanbas/Calendar">Calendar</a></li>
-      <li><a id="wd-inbox-link" href="#/Kanbas/Inbox">Inbox</a></li>
-      <li><a id="wd-labs-link" href="#/Labs">Labs</a></li>
-      <li><a id="wd-landing-page-link" href="#/Kanbas/LandingPage">Landing Page</a></li> {/* Add link to Landing Page */}
-    </ul>
   );
 }
