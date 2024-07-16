a69840a77f303d65b6ba4701031db5ae635f0400
tcsimm <widet77@icloud.com>
2024-07-16 07:07:17 -0400

    updates for regrade

--- src/Kanbas/Courses/Assignments/editor.tsx
+++ src/Kanbas/Courses/Assignments/editor.tsx
@@ -1,99 +1,127 @@
+import React from 'react';
+
 export default function AssignmentEditor() {
-    return (
-      <div id="wd-assignments-editor">
+  return (
+    <div id="wd-assignments-editor">
+      <h2>Assignment Editor</h2>
+      <div>
         <label htmlFor="wd-name">Assignment Name</label>
         <input id="wd-name" value="A1 - ENV + HTML" /><br /><br />
+        <label htmlFor="wd-description">Details</label>
         <textarea id="wd-description">
-          The assignment is available online Submit a link to the landing page of your Web application running on Netlify. The landing page should include the following: Your full name and section Links to each of the lab assignments Link to the Kanbas application Links to all relevant source code repositories The Kanbas application should include a link to navigate back to the landing page.
+          The assignment is available online Submit a link to the landing page of your Web application running on Netlify. The landing page should include the following:
+          - Your full name and section
+          - Links to each of the lab assignments
+          - Link to the Kanbas application
+          - Links to all relevant source code repositories
         </textarea>
         <br />
         <table>
-          <tr>
-            <td align="right" valign="top">
-              <label htmlFor="wd-points">Points</label>
-            </td>
-            <td>
-              <input id="wd-points" value={100} />
-            </td>
-          </tr>
-          <tr>
-            <td align="right" valign="top">
-              <label htmlFor="wd-group">Assignment Group</label>
-            </td>
-            <td>
-              <select id="wd-group">
-                <option value="assignments">ASSIGNMENTS</option>
-              </select>
-            </td>
-          </tr>
-          <tr>
-            <td align="right" valign="top">
-              <label htmlFor="wd-display-grade-as">Display Grade as</label>
-            </td>
-            <td>
-              <select id="wd-display-grade-as">
-                <option value="percentage">Percentage</option>
-              </select>
-            </td>
-          </tr>
-          <tr>
-            <td align="right" valign="top">
-              <label htmlFor="wd-submission-type">Submission Type</label>
-            </td>
-            <td>
-              <select id="wd-submission-type">
-                <option value="online">Online</option>
-              </select>
-              <div>
-                <input type="checkbox" id="wd-text-entry" />
-                <label htmlFor="wd-text-entry">Text Entry</label><br />
-                <input type="checkbox" id="wd-website-url" />
-                <label htmlFor="wd-website-url">Website URL</label><br />
-                <input type="checkbox" id="wd-media-recordings" />
-                <label htmlFor="wd-media-recordings">Media Recordings</label><br />
-                <input type="checkbox" id="wd-student-annotation" />
-                <label htmlFor="wd-student-annotation">Student Annotation</label><br />
-                <input type="checkbox" id="wd-file-upload" />
-                <label htmlFor="wd-file-upload">File Uploads</label>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td align="right" valign="top">
-              <label htmlFor="wd-assign-to">Assign to</label>
-            </td>
-            <td>
-              <input id="wd-assign-to" value="Everyone" />
-            </td>
-          </tr>
-          <tr>
-            <td align="right" valign="top">
-              <label htmlFor="wd-due-date">Due</label>
-            </td>
-            <td>
-              <input type="date" id="wd-due-date" value="2024-05-13" />
-            </td>
-          </tr>
-          <tr>
-            <td align="right" valign="top">
-              <label htmlFor="wd-available-from">Available from</label>
-            </td>
-            <td>
-              <input type="date" id="wd-available-from" value="2024-05-06" />
-            </td>
-          </tr>
-          <tr>
-            <td align="right" valign="top">
-              <label htmlFor="wd-available-until">Until</label>
-            </td>
-            <td>
-              <input type="date" id="wd-available-until" value="2024-05-20" />
-            </td>
-          </tr>
+          <tbody>
+            <tr>
+              <td align="right" valign="top">
+                <label htmlFor="wd-points">Points</label>
+              </td>
+              <td>
+                <input id="wd-points" type="number" value={100} />
+              </td>
+            </tr>
+            <tr>
+              <td align="right" valign="top">
+                <label htmlFor="wd-group">Assignment Group</label>
+              </td>
+              <td>
+                <select id="wd-group">
+                  <option value="Assignments">Assignments</option>
+                  {/* Add other groups as needed */}
+                </select>
+              </td>
+            </tr>
+            <tr>
+              <td align="right" valign="top">
+                <label htmlFor="wd-display-grade-as">Display Grade as</label>
+              </td>
+              <td>
+                <select id="wd-display-grade-as">
+                  <option value="Percentage">Percentage</option>
+                  <option value="Points">Points</option>
+                  {/* Add other grading methods as needed */}
+                </select>
+              </td>
+            </tr>
+            <tr>
+              <td align="right" valign="top">
+                <label htmlFor="wd-submission-type">Submission Type</label>
+              </td>
+              <td>
+                <select id="wd-submission-type">
+                  <option value="Online">Online</option>
+                  {/* Add other submission types as needed */}
+                </select>
+              </td>
+            </tr>
+          </tbody>
         </table>
-        <button>Cancel</button>
-        <button>Save</button>
+        <br />
+        <fieldset>
+          <legend>Online Entry Options</legend>
+          <label htmlFor="wd-text-entry">
+            <input type="checkbox" id="wd-text-entry" /> Text Entry
+          </label><br />
+          <label htmlFor="wd-website-url">
+            <input type="checkbox" id="wd-website-url" /> Website URL
+          </label><br />
+          <label htmlFor="wd-media-recordings">
+            <input type="checkbox" id="wd-media-recordings" /> Media Recordings
+          </label><br />
+          <label htmlFor="wd-student-annotation">
+            <input type="checkbox" id="wd-student-annotation" /> Student Annotation
+          </label><br />
+          <label htmlFor="wd-file-upload">
+            <input type="checkbox" id="wd-file-upload" /> File Uploads
+          </label><br />
+        </fieldset>
+        <br />
+        <table>
+          <tbody>
+            <tr>
+              <td align="right" valign="top">
+                <label htmlFor="wd-assign-to">Assign to</label>
+              </td>
+              <td>
+                <input id="wd-assign-to" value="Everyone" />
+              </td>
+            </tr>
+            <tr>
+              <td align="right" valign="top">
+                <label htmlFor="wd-due-date">Due Date</label>
+              </td>
+              <td>
+                <input id="wd-due-date" type="date" value="2024-05-13" />
+              </td>
+            </tr>
+            <tr>
+              <td align="right" valign="top">
+                <label htmlFor="wd-available-from">Available from</label>
+              </td>
+              <td>
+                <input id="wd-available-from" type="date" value="2024-05-06" />
+              </td>
+            </tr>
+            <tr>
+              <td align="right" valign="top">
+                <label htmlFor="wd-available-until">Available until</label>
+              </td>
+              <td>
+                <input id="wd-available-until" type="date" value="2024-05-20" />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <br />
+        <button type="button">Save</button>
+        <button type="button">Cancel</button>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,37 +1,41 @@
+import React from 'react';
 import CoursesNavigation from "./Navigation";
 import Modules from "./Modules";
 import Home from "./Home";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/editor";
-import { Navigate, Route, Routes } from "react-router";
+import { Navigate, Route, Routes, useParams, useLocation } from "react-router";
+import { FaAlignJustify } from "react-icons/fa";
+import "../styles.css";
 
 export default function Courses() {
+  const { cid } = useParams();
+  const { pathname } = useLocation();
+  const section = pathname.split("/")[4];
+
   return (
-    <div id="wd-courses">
-      <h2>Course 1234</h2>
+    <div id="wd-courses" className="container-fluid">
+      <h2 className="text-danger">
+        <FaAlignJustify className="me-3 fs-4 mb-1" />
+      </h2>
       <hr />
-      <table>
-        <tbody>
-          <tr>
-            <td valign="top">
-              <CoursesNavigation />
-            </td>
-            <td valign="top">
-              <Routes>
-                <Route path="/" element={<Navigate to="Home" />} />
-                <Route path="Home" element={<Home />} />
-                <Route path="Modules" element={<Modules />} />
-                <Route path="Piazza" element={<h3>Piazza</h3>} />
-                <Route path="Zoom" element={<h4>Zoom</h4>} />
-                <Route path="Assignments" element={<Assignments />} />
-                <Route path="Assignments/:id" element={<AssignmentEditor />} />
-                <Route path="Quizzes" element={<h6>Quizzes</h6>} />
-                <Route path="Grades" element={<h6>Grades</h6>} />
-              </Routes>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="d-flex">
+        <div className="me-3">
+          <CoursesNavigation />
+        </div>
+        <div className="flex-grow-1">
+          <Routes>
+            <Route path="/" element={<Navigate to="Home" />} />
+            <Route path="Home" element={<Home />} />
+            <Route path="Modules" element={<Modules />} />
+            <Route path="Piazza" element={<h3>Piazza</h3>} />
+            <Route path="Zoom" element={<h4>Zoom</h4>} />
+            <Route path="Assignments" element={<Assignments />} />
+            <Route path="Assignments/:id" element={<AssignmentEditor />} />
+            <Route path="Quizzes" element={<h6>Quizzes</h6>} />
+          </Routes>
+        </div>
+      </div>
     </div>
   );
 }
