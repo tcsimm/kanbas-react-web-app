9290bae5e079e6f84ecae834234c1e319b254b14
tcsimm <widet77@icloud.com>
2024-07-16 10:42:23 -0400

    again

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -4,6 +4,21 @@ import Dashboard from "./Dashboard";
 import Courses from "./Courses";
 import LandingPage from "../LandingPage";
 
+function KanbasNavigation() {
+  return (
+    <ul id="wd-kanbas-navigation">
+      <li><a id="wd-neu-link" target="_blank" rel="noreferrer" href="https://www.northeastern.edu/">Northeastern</a></li>
+      <li><a id="wd-account-link" href="#/Kanbas/Account">Account</a></li>
+      <li><a id="wd-dashboard-link" href="#/Kanbas/Dashboard">Dashboard</a></li>
+      <li><a id="wd-course-link" href="#/Kanbas/Courses">Courses</a></li>
+      <li><a id="wd-calendar-link" href="#/Kanbas/Calendar">Calendar</a></li>
+      <li><a id="wd-inbox-link" href="#/Kanbas/Inbox">Inbox</a></li>
+      <li><a id="wd-labs-link" href="#/Labs">Labs</a></li>
+      <li><a id="wd-landing-page-link" href="#/Kanbas/LandingPage">Landing Page</a></li> {/* Add link to Landing Page */}
+    </ul>
+  );
+}
+
 export default function Kanbas() {
   return (
     <div id="wd-kanbas">
@@ -25,20 +40,5 @@ export default function Kanbas() {
         </tbody>
       </table>
     </div>
-  );
-}
-
-function KanbasNavigation() {
-  return (
-    <ul id="wd-kanbas-navigation">
-      <li><a id="wd-neu-link" target="_blank" rel="noreferrer" href="https://www.northeastern.edu/">Northeastern</a></li>
-      <li><a id="wd-account-link" href="#/Kanbas/Account">Account</a></li>
-      <li><a id="wd-dashboard-link" href="#/Kanbas/Dashboard">Dashboard</a></li>
-      <li><a id="wd-course-link" href="#/Kanbas/Courses">Courses</a></li>
-      <li><a id="wd-calendar-link" href="#/Kanbas/Calendar">Calendar</a></li>
-      <li><a id="wd-inbox-link" href="#/Kanbas/Inbox">Inbox</a></li>
-      <li><a id="wd-labs-link" href="#/Labs">Labs</a></li>
-      <li><a id="wd-landing-page-link" href="#/Kanbas/LandingPage">Landing Page</a></li> {/* Add link to Landing Page */}
-    </ul>
   );
 }
