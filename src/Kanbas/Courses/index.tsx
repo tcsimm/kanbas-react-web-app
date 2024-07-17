35490142e9cd00ead4df367c4f752f3394346090
tcsimm <widet77@icloud.com>
2024-07-17 19:29:04 -0400

    fix

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -32,7 +32,7 @@ export default function Courses() {
             <Route path="Home" element={<Home />} />
             <Route path="Modules" element={<Modules />} />
             <Route path="Assignments" element={<Assignments />} />
-            <Route path="Assignments/:id" element={<AssignmentEditor />} />
+            <Route path="Assignments/:aid" element={<AssignmentEditor />} />
             <Route path="Piazza" element={<h3>Piazza</h3>} />
             <Route path="Zoom" element={<h4>Zoom</h4>} />
             <Route path="Quizzes" element={<h6>Quizzes</h6>} />
