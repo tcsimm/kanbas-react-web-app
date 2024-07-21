6bdf7a501e7da5cd4e6d77a4a3be4dbd93e790f2
tcsimm <widet77@icloud.com>
2024-07-20 21:54:35 -0400

    possible assignment update

--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -1,23 +1,49 @@
 import React from 'react';
 import { Link, useParams } from 'react-router-dom';
-import * as db from "../../Database";
+import { FaPlus, FaEllipsisV, FaCheckCircle } from 'react-icons/fa';
+import * as db from '../../Database';
+import './Assignments.css'; // Ensure you create and import this CSS file
 
 const Assignments = () => {
   const { cid } = useParams();
   const assignments = db.assignments.filter(assignment => assignment.course === cid);
 
   return (
-    <div id="wd-assignments">
-      <h1>Assignments</h1>
-      <ul className="list-group">
-        {assignments.map(assignment => (
-          <li key={assignment._id} className="list-group-item">
-            <Link to={`/Kanbas/Courses/${cid}/Assignments/${assignment._id}`}>
-              {assignment.title}
-            </Link>
-          </li>
+    <div className="container mt-4">
+      <h2 className="text-danger">Assignments</h2>
+      <hr />
+      <div className="d-flex justify-content-between align-items-center mb-3">
+        <input type="text" className="form-control search-bar" placeholder="Search..." />
+        <div>
+          <button className="btn btn-outline-secondary me-2">+ Group</button>
+          <button className="btn btn-danger">+ Assignment</button>
+        </div>
+      </div>
+      <div className="assignments-list">
+        {assignments.map((assignment) => (
+          <div key={assignment._id} className="assignment-item">
+            <div className="d-flex justify-content-between align-items-center">
+              <div className="d-flex align-items-center">
+                <div className="drag-handle me-3">
+                  <FaEllipsisV />
+                </div>
+                <div>
+                  <Link to={`/Kanbas/Courses/${cid}/Assignments/${assignment._id}`} className="assignment-title">
+                    {assignment.title}
+                  </Link>
+                  <div className="assignment-details">
+                    Multiple Modules | Not available until May 6 at 12:00am | Due May 13 at 11:59pm | 100 pts
+                  </div>
+                </div>
+              </div>
+              <div className="d-flex align-items-center">
+                <FaCheckCircle className="check-circle me-3" />
+                <FaEllipsisV />
+              </div>
+            </div>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
