77548f670c74bc432618c7fefd8240ee98e11e8d
tcsimm <widet77@icloud.com>
2024-07-22 11:00:49 -0400

    back

--- src/Labs/Lab4/ReduxExamples/HelloRedux/index.tsx
+++ src/Labs/Lab4/ReduxExamples/HelloRedux/index.tsx
@@ -1,14 +1,10 @@
-import React from "react";
-import { useSelector } from "react-redux";
-
+import { useSelector, useDispatch } from "react-redux";
 export default function HelloRedux() {
-  const message = useSelector((state: any) => state.helloReducer.message);
-
+  const { message } = useSelector((state: any) => state.helloReducer);
   return (
     <div id="wd-hello-redux">
       <h3>Hello Redux</h3>
-      <h4>{message}</h4>
-      <hr />
+      <h4>{message}</h4> <hr />
     </div>
   );
 }
