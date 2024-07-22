9fe74d673d6a34e60f2967d60b267ac16216c6f2
tcsimm <widet77@icloud.com>
2024-07-22 07:50:32 -0400

    fixes

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,8 +1,12 @@
+import React from "react";
+import ClickEvent from "./ClickEvent"; // Ensure this path is correct
+
 export default function Lab4() {
-    return (
-      <div id="wd-lab3" className="container-fluid">
-        <h3>Lab 4</h3>
-      </div>
-    );
-  }
-  
+  return (
+    <div id="wd-lab4" className="container">
+      <h2>Lab 4 - Handling User Events</h2>
+      <h3>Handling Click Events</h3>
+      <ClickEvent />
+    </div>
+  );
+}
