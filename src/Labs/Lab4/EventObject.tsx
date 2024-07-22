d151d76f11e5ff20eec862f7c7d2102129a9f1d7
tcsimm <widet77@icloud.com>
2024-07-22 08:24:51 -0400

    EventObject

--- src/Labs/Lab4/EventObject.tsx
+++ src/Labs/Lab4/EventObject.tsx
@@ -1,27 +1,21 @@
 import React, { useState } from "react";
-
 export default function EventObject() {
-  const [event, setEvent] = useState<any>(null);
-
+  const [event, setEvent] = useState(null);
   const handleClick = (e: any) => {
-    const eventCopy = { ...e };
-    eventCopy.target = e.target.outerHTML;
-    delete eventCopy.view;
-    setEvent(eventCopy);
+    e.target = e.target.outerHTML;
+    delete e.view;
+    setEvent(e);
   };
-
   return (
     <div>
       <h2>Event Object</h2>
-      <button
-        onClick={(e) => handleClick(e)}
+      <button onClick={(e) => handleClick(e)}
         className="btn btn-primary"
-        id="wd-display-event-obj-click"
-      >
+        id="wd-display-event-obj-click">
         Display Event Object
       </button>
       <pre>{JSON.stringify(event, null, 2)}</pre>
-      <hr />
+      <hr/>
     </div>
-  );
-}
+  );}
+
