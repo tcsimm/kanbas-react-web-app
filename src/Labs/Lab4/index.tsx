8a2f63343d0064c0356e17df5fb7994f06db2691
tcsimm <widet77@icloud.com>
2024-07-22 08:06:56 -0400

    PassingFunctions

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,14 +1,22 @@
-import React from "react";
-import ClickEvent from "./ClickEvent";
-import PassingDataOnEvent from "./PassingDataOnEvent";
+import React from 'react';
+import ClickEvent from './ClickEvent'; // Ensure this path is correct
+import PassingDataOnEvent from './PassingDataOnEvent'; // Ensure this path is correct
+import PassingFunctions from './PassingFunctions'; // Ensure this path is correct
 
 export default function Lab4() {
+  function sayHello() {
+    alert("Hello");
+  }
+
   return (
     <div id="wd-lab4" className="container">
       <h2>Lab 4 - Handling User Events</h2>
       <h3>Handling Click Events</h3>
       <ClickEvent />
+      <h3>Passing Data on Event</h3>
       <PassingDataOnEvent />
+      <h3>Passing Functions as Parameters</h3>
+      <PassingFunctions theFunction={sayHello} />
     </div>
   );
 }
