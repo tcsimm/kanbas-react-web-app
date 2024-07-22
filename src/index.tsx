8f1a330bb2e4cce65d1ecf545549170d6b565530
tcsimm <widet77@icloud.com>
2024-07-22 10:53:50 -0400

    redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./Labs/store"; // Ensure the path is correct
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
-reportWebVitals();
 
+reportWebVitals();
