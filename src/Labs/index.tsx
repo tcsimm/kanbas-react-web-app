984b6622022af395f7bf87235fdf1ae431d4d2f1
tcsimm <widet77@icloud.com>
2024-07-22 12:13:33 -0400

    routes

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -4,10 +4,12 @@ import TOC from "./TOC";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
 import Lab4 from "./Lab4";
+import store from "./store";
 import { Provider } from "react-redux";
 
 export default function Labs() {
   return (
+    <Provider store={store}>
     <div>
       <h1>Labs</h1>
       <h2>Thomas Simmons</h2>
@@ -20,5 +22,6 @@ export default function Labs() {
         <Route path="Lab4" element={<Lab4 />} />
       </Routes>
     </div>
+    </Provider>
   );
 }
