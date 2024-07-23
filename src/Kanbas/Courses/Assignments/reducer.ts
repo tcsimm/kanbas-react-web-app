b38ca2f612d75717211f4e09af44824bf5e268b5
tcsimm <widet77@icloud.com>
2024-07-22 22:14:22 -0400

    3.4

---
+++ src/Kanbas/Courses/Assignments/reducer.ts
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { assignments } from "../../Database";
+
+const initialState = {
+  assignments: assignments,
+};
+
+const assignmentsSlice = createSlice({
+  name: "assignments",
+  initialState,
+  reducers: {
+    addAssignment: (state, { payload: assignment }) => {
+      const newAssignment = {
+        _id: new Date().getTime().toString(),
+        ...assignment,
+      };
+      state.assignments = [...state.assignments, newAssignment];
+    },
+    deleteAssignment: (state, { payload: assignmentId }) => {
+      state.assignments = state.assignments.filter(
+        (a) => a._id !== assignmentId
+      );
+    },
+    updateAssignment: (state, { payload: assignment }) => {
+      state.assignments = state.assignments.map((a) =>
+        a._id === assignment._id ? assignment : a
+      );
+    },
+  },
+});
+
+export const { addAssignment, deleteAssignment, updateAssignment } =
+  assignmentsSlice.actions;
+export default assignmentsSlice.reducer;
