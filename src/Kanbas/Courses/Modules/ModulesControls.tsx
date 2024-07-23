3da0cafdfd0584765083d1ddf2935888228a0333
tcsimm <widet77@icloud.com>
2024-07-22 20:25:50 -0400

    3.3.1 ModulesControls

--- src/Kanbas/Courses/Modules/ModulesControls.tsx
+++ src/Kanbas/Courses/Modules/ModulesControls.tsx
@@ -8,20 +8,20 @@ export default function ModulesControls(
 
   return (
     <div id="wd-modules-controls" className="text-nowrap">
-      <button
-        id="wd-add-module-btn"
-        className="btn btn-lg btn-danger me-1 float-end"
-        data-bs-toggle="modal"
+      <button 
+        id="wd-add-module-btn" 
+        className="btn btn-lg btn-danger me-1 float-end" 
+        data-bs-toggle="modal" 
         data-bs-target="#wd-add-module-dialog"
       >
         <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
         Module
       </button>
       <div className="dropdown d-inline me-1 float-end">
-        <button
-          id="wd-publish-all-btn"
+        <button 
+          id="wd-publish-all-btn" 
           className="btn btn-lg btn-secondary dropdown-toggle"
-          type="button"
+          type="button" 
           data-bs-toggle="dropdown"
         >
           <GreenCheckmark />
@@ -58,11 +58,11 @@ export default function ModulesControls(
       <button id="wd-collapse-all" className="btn btn-lg btn-secondary me-1 float-end">
         Collapse All
       </button>
-      <ModuleEditor
-        dialogTitle="Add Module"
-        moduleName={moduleName}
-        setModuleName={setModuleName}
-        addModule={addModule}
+      <ModuleEditor 
+        dialogTitle="Add Module" 
+        moduleName={moduleName} 
+        setModuleName={setModuleName} 
+        addModule={addModule} 
       />
     </div>
   );
