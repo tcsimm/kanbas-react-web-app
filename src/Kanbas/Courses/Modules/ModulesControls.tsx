80211841d366ec301d76cdac10881a0db08836e7
tcsimm <widet77@icloud.com>
2024-07-23 16:16:12 -0400

    delete

--- src/Kanbas/Courses/Modules/ModulesControls.tsx
+++ src/Kanbas/Courses/Modules/ModulesControls.tsx
@@ -11,13 +11,14 @@ export default function ModulesControls(
       <button
         id="wd-add-module-btn"
         className="btn btn-lg btn-danger me-1 float-end"
-        data-bs-toggle="modal" data-bs-target="#wd-add-module-dialog" >
+        data-bs-toggle="modal"
+        data-bs-target="#wd-add-module-dialog">
         <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
         Module
       </button>
       <div className="dropdown d-inline me-1 float-end">
         <button id="wd-publish-all-btn" className="btn btn-lg btn-secondary dropdown-toggle"
-          type="button" data-toggle="dropdown">
+          type="button" data-bs-toggle="dropdown">
           <GreenCheckmark />
           Publish All
         </button>
