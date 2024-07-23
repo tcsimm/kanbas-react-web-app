996ca07e15a063e04e92a0a79bca1d0f611b70e3
tcsimm <widet77@icloud.com>
2024-07-22 21:31:33 -0400

    3.3.4 index modules

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -1,6 +1,8 @@
+// src/Kanbas/Courses/Modules/index.tsx
 import React, { useState } from "react";
 import { useParams } from "react-router";
-import * as db from "../../Database";
+import { useSelector, useDispatch } from "react-redux";
+import { addModule, editModule, updateModule, deleteModule } from "./reducer";
 import { BsGripVertical } from "react-icons/bs";
 import ModuleControlButtons from "./ModuleControlButtons";
 import LessonControlButtons from "./LessonControlButtons";
@@ -8,39 +10,25 @@ import ModulesControls from "./ModulesControls";
 
 export default function Modules() {
   const { cid } = useParams();
-  const [modules, setModules] = useState<any[]>(db.modules);
-  const [moduleName, setModuleName] = useState<string>("");
-
-  const addModule = () => {
-    const newModule = { _id: new Date().getTime().toString(), name: moduleName, course: cid, lessons: [] };
-    setModules([...modules, newModule]);
-    setModuleName("");
-  };
-
-  const deleteModule = (moduleId: string) => {
-    setModules(modules.filter((m) => m._id !== moduleId));
-  };
-
-  const editModule = (moduleId: string) => {
-    setModules(modules.map((m) => (m._id === moduleId ? { ...m, editing: true } : m)));
-  };
-
-  const updateModule = (module: any) => {
-    setModules(modules.map((m) => (m._id === module._id ? module : m)));
-  };
+  const [moduleName, setModuleName] = useState("");
+  const { modules } = useSelector((state: any) => state.modules);
+  const dispatch = useDispatch();
 
   return (
     <div id="wd-modules">
-      <ModulesControls 
+      <ModulesControls
         moduleName={moduleName}
         setModuleName={setModuleName}
-        addModule={addModule}
+        addModule={() => {
+          dispatch(addModule({ name: moduleName, course: cid }));
+          setModuleName("");
+        }}
       />
       <br /><br /><br />
       <ul id="wd-modules" className="list-group rounded-0">
         {modules
-          .filter((module) => module.course === cid)
-          .map((module) => (
+          .filter((module: any) => module.course === cid)
+          .map((module: any) => (
             <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray" key={module._id}>
               <div className="wd-title p-3 ps-2 bg-secondary">
                 <BsGripVertical className="me-2 fs-3" />
@@ -48,10 +36,10 @@ export default function Modules() {
                 {module.editing && (
                   <input
                     className="form-control w-50 d-inline-block"
-                    onChange={(e) => updateModule({ ...module, name: e.target.value })}
+                    onChange={(e) => dispatch(updateModule({ ...module, name: e.target.value }))}
                     onKeyDown={(e) => {
                       if (e.key === "Enter") {
-                        updateModule({ ...module, editing: false });
+                        dispatch(updateModule({ ...module, editing: false }));
                       }
                     }}
                     value={module.name}
@@ -59,8 +47,10 @@ export default function Modules() {
                 )}
                 <ModuleControlButtons
                   moduleId={module._id}
-                  deleteModule={deleteModule}
-                  editModule={editModule}
+                  deleteModule={(moduleId) => {
+                    dispatch(deleteModule(moduleId));
+                  }}
+                  editModule={(moduleId) => dispatch(editModule(moduleId))}
                 />
               </div>
               {module.lessons && (
