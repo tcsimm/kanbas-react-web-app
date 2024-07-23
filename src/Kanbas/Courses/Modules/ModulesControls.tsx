7006cf6585754b9c1a127fac0885b3314d060840
tcsimm <widet77@icloud.com>
2024-07-22 20:14:21 -0400

    3.3.1 red button fix

--- src/Kanbas/Courses/Modules/ModulesControls.tsx
+++ src/Kanbas/Courses/Modules/ModulesControls.tsx
@@ -1,4 +1,4 @@
-import { FaPlus } from "react-icons/fa6";
+import { FaPlus } from "react-icons/fa";
 import GreenCheckmark from "./GreenCheckmark";
 import ModuleEditor from "./ModuleEditor";
 
@@ -8,14 +8,22 @@ export default function ModulesControls(
 
   return (
     <div id="wd-modules-controls" className="text-nowrap">
-      <button id="wd-add-module-btn" className="btn btn-lg btn-danger me-1 float-end">
-      data-bs-toggle="modal" data-bs-target="#wd-add-module-dialog" 
+      <button
+        id="wd-add-module-btn"
+        className="btn btn-lg btn-danger me-1 float-end"
+        data-bs-toggle="modal"
+        data-bs-target="#wd-add-module-dialog"
+      >
         <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
         Module
       </button>
       <div className="dropdown d-inline me-1 float-end">
-        <button id="wd-publish-all-btn" className="btn btn-lg btn-secondary dropdown-toggle"
-          type="button" data-bs-toggle="dropdown">
+        <button
+          id="wd-publish-all-btn"
+          className="btn btn-lg btn-secondary dropdown-toggle"
+          type="button"
+          data-bs-toggle="dropdown"
+        >
           <GreenCheckmark />
           Publish All
         </button>
@@ -50,8 +58,12 @@ export default function ModulesControls(
       <button id="wd-collapse-all" className="btn btn-lg btn-secondary me-1 float-end">
         Collapse All
       </button>
-      <ModuleEditor dialogTitle="Add Module" moduleName={moduleName}
-                    setModuleName={setModuleName} addModule={addModule} />
+      <ModuleEditor
+        dialogTitle="Add Module"
+        moduleName={moduleName}
+        setModuleName={setModuleName}
+        addModule={addModule}
+      />
     </div>
   );
 }
