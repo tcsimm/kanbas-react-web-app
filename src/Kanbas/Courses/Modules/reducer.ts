a6231e7cd21e01a17224ac7cb47186a79a59b5cf
tcsimm <widet77@icloud.com>
2024-07-23 22:19:40 -0400

    revert

--- src/Kanbas/Courses/Modules/reducer.ts
+++ src/Kanbas/Courses/Modules/reducer.ts
@@ -5,38 +5,33 @@ const initialState = {
   modules: modules,
 };
 
-const defaultLessons = [
-  { name: "Lesson 1" },
-  { name: "Lesson 2" },
-  { name: "Lesson 3" }
-];
-
 const modulesSlice = createSlice({
   name: "modules",
   initialState,
   reducers: {
     addModule: (state, { payload: module }) => {
       const newModule: any = {
-        id: new Date().getTime().toString(),
-        lessons: defaultLessons,
+        _id: new Date().getTime().toString(),
+        lessons: [],
         name: module.name,
         course: module.course,
       };
-      state.modules = [...state.modules, newModule];
+      state.modules = [...state.modules, newModule] as any;
     },
     deleteModule: (state, { payload: moduleId }) => {
-      const newModules = state.modules.filter((m: any) => m.id !== moduleId);
-      state.modules = newModules;
+      console.log(state.modules)
+      const newModules = state.modules.filter((m: any) => m._id !== moduleId);
+      state.modules = newModules
     },
     updateModule: (state, { payload: module }) => {
       state.modules = state.modules.map((m: any) =>
-        m.id === module.id ? module : m
-      );
+        m._id === module._id ? module : m
+      ) as any;
     },
     editModule: (state, { payload: moduleId }) => {
       state.modules = state.modules.map((m: any) =>
-        m.id === moduleId ? { ...m, editing: true } : m
-      );
+        m._id === moduleId ? { ...m, editing: true } : m
+      ) as any;
     },
   },
 });
