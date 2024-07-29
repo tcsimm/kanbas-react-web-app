339da091a80f0011a5f2be4f9663d89829e37629
tcsimm <widet77@icloud.com>
2024-07-29 08:47:30 -0400

    react

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -1,15 +1,16 @@
+import EnvironmentVariables from "./EnvironmentVariables";
 export default function Lab5() {
-    return (
-      <div id="wd-lab5">
-        <h2>Lab 5</h2>
-        <div className="list-group">
-          <a href="http://localhost:4000/lab5/welcome"          
-             className="list-group-item">
-             Welcome
-          </a>
-        </div><hr/>
-      </div>
-    );
-  }
-  
-  
+  return (
+    <div id="wd-lab5">
+      <h2>Lab 5</h2>
+      <div className="list-group">
+        <a href="http://localhost:4000/lab5/welcome"          
+           className="list-group-item">
+           Welcome
+        </a>
+      </div><hr/>
+      <EnvironmentVariables />
+    </div>
+  );
+}
+
