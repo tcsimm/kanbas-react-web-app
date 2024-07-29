1ab465e9799b8e839b6c6faf777af039b6428ff7
tcsimm <widet77@icloud.com>
2024-07-29 16:35:59 -0400

    fix#	modified:   src/Labs/Lab5/index.tsx

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -1,28 +1,16 @@
-import React from 'react';
-
+import EnvironmentVariables from "./EnvironmentVariables";
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
-
 export default function Lab5() {
   return (
     <div id="wd-lab5">
       <h2>Lab 5</h2>
       <div className="list-group">
-        <a href="http://localhost:4000/lab5/welcome"          
-           className="list-group-item">
-           Welcome
+        <a href={`${REMOTE_SERVER}/lab5/welcome`} className="list-group-item">
+          Welcome
         </a>
-      </div>
-      <hr/>
+      </div><hr />
       <EnvironmentVariables />
     </div>
   );
 }
 
-function EnvironmentVariables() {
-  return (
-    <div id="wd-environment-variables">
-      <h3>Environment Variables</h3>
-      <p>Remote Server: {REMOTE_SERVER}</p><hr/>
-    </div>
-  );
-}
