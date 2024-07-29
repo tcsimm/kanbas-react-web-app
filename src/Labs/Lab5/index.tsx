52df92654bdddb8e6ec6c0a70f961bfa53df2b9d
tcsimm <widet77@icloud.com>
2024-07-29 16:35:07 -0400

    EnvironmentVariables

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -1,13 +1,28 @@
+import React from 'react';
+
+const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
+
 export default function Lab5() {
   return (
     <div id="wd-lab5">
       <h2>Lab 5</h2>
       <div className="list-group">
-        <a href="http://localhost:4000/lab5/welcome" className="list-group-item">
-          Welcome
+        <a href="http://localhost:4000/lab5/welcome"          
+           className="list-group-item">
+           Welcome
         </a>
       </div>
-      <hr />
+      <hr/>
+      <EnvironmentVariables />
+    </div>
+  );
+}
+
+function EnvironmentVariables() {
+  return (
+    <div id="wd-environment-variables">
+      <h3>Environment Variables</h3>
+      <p>Remote Server: {REMOTE_SERVER}</p><hr/>
     </div>
   );
 }
