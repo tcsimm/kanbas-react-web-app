96777cc5bf38b5721c73d287358e72dd166531ed
tcsimm <widet77@icloud.com>
2024-07-30 09:01:28 -0400

    3.5.6

--- src/Labs/Lab5/client.ts
+++ src/Labs/Lab5/client.ts
@@ -7,7 +7,6 @@ export const fetchWelcomeMessage = async () => {
 };
 
 const ASSIGNMENT_API = `${REMOTE_SERVER}/lab5/assignment`;
-
 export const fetchAssignment = async () => {
   const response = await axios.get(`${ASSIGNMENT_API}`);
   return response.data;
@@ -15,5 +14,11 @@ export const fetchAssignment = async () => {
 
 export const updateTitle = async (title: string) => {
   const response = await axios.get(`${ASSIGNMENT_API}/title/${title}`);
+  return response.data;
+};
+
+const TODOS_API = `${REMOTE_SERVER}/lab5/todos`;
+export const fetchTodos = async () => {
+  const response = await axios.get(TODOS_API);
   return response.data;
 };
