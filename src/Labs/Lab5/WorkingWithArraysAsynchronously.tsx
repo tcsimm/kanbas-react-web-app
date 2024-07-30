8d40c68dbed94c0547524ea1049a6a80733c4127
tcsimm <widet77@icloud.com>
2024-07-30 14:26:00 -0400

    3.6.3

--- src/Labs/Lab5/WorkingWithArraysAsynchronously.tsx
+++ src/Labs/Lab5/WorkingWithArraysAsynchronously.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { FaPlusCircle, FaTrash } from "react-icons/fa";
+import { FaPlusCircle, FaTrash, FaEdit } from "react-icons/fa";
 import { TiDelete } from "react-icons/ti";
 import * as client from "./client";
 
@@ -32,6 +32,16 @@ export default function WorkingWithArraysAsynchronously() {
         setTodos(newTodos);
     };
 
+    const editTodo = (todo: any) => {
+        const updatedTodos = todos.map((t) => (t.id === todo.id ? { ...todo, editing: true } : t));
+        setTodos(updatedTodos);
+    };
+
+    const updateTodo = async (todo: any) => {
+        await client.updateTodo(todo);
+        setTodos(todos.map((t) => (t.id === todo.id ? todo : t)));
+    };
+
     useEffect(() => {
         fetchTodos();
     }, []);
@@ -47,10 +57,25 @@ export default function WorkingWithArraysAsynchronously() {
             <ul className="list-group">
                 {todos.map((todo) => (
                     <li key={todo.id} className="list-group-item">
+                        <FaEdit onClick={() => editTodo(todo)} className="text-primary float-end me-2 mt-1" />
                         <FaTrash onClick={() => removeTodo(todo)} className="text-danger float-end mt-1" id="wd-remove-todo" />
                         <TiDelete onClick={() => deleteTodo(todo)} className="text-danger float-end me-2 fs-3" id="wd-delete-todo" />
-                        <input type="checkbox" className="form-check-input me-2" defaultChecked={todo.completed} />
-                        <span style={{ textDecoration: todo.completed ? "line-through" : "none" }}>{todo.title}</span>
+                        <input type="checkbox" defaultChecked={todo.completed} className="form-check-input me-2 float-start"
+                            onChange={(e) => updateTodo({ ...todo, completed: e.target.checked }) } />
+                        {!todo.editing ? (
+                            <span style={{ textDecoration: todo.completed ? "line-through" : "none" }}>{todo.title}</span>
+                        ) : (
+                            <input className="form-control w-50 float-start" defaultValue={todo.title}
+                                onKeyDown={(e) => {
+                                    if (e.key === "Enter") {
+                                        updateTodo({ ...todo, editing: false });
+                                    }
+                                }}
+                                onChange={(e) =>
+                                    updateTodo({ ...todo, title: e.target.value })
+                                }
+                            />
+                        )}
                     </li>
                 ))}
             </ul>
