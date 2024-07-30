01f1ff4dc307756aadd0c6032249e4e933d3dc36
tcsimm <widet77@icloud.com>
2024-07-30 08:43:20 -0400

    3.4.6

--- src/Labs/Lab5/WorkingWithArrays.tsx
+++ src/Labs/Lab5/WorkingWithArrays.tsx
@@ -3,7 +3,10 @@ const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 
 export default function WorkingWithArrays() {
   const API = `${REMOTE_SERVER}/lab5/todos`;
-  const [todo, setTodo] = useState({ id: "1" });
+  const [todo, setTodo] = useState({
+    id: "1",
+    title: "Task Title",
+  });
 
   return (
     <div id="wd-working-with-arrays">
@@ -55,6 +58,27 @@ export default function WorkingWithArrays() {
         className="form-control w-50"
         onChange={(e) => setTodo({ ...todo, id: e.target.value })}
       />
+      <hr />
+      <h4>Updating an Item in an Array</h4>
+      <a
+        id="wd-update-todo"
+        className="btn btn-primary float-end"
+        href={`${API}/${todo.id}/title/${todo.title}`}
+      >
+        Update Todo
+      </a>
+      <input
+        value={todo.id}
+        className="form-control w-25 float-start me-2"
+        onChange={(e) => setTodo({ ...todo, id: e.target.value })}
+      />
+      <input
+        value={todo.title}
+        className="form-control w-50 float-start"
+        onChange={(e) => setTodo({ ...todo, title: e.target.value })}
+      />
+      <br />
+      <br />
       <hr />
     </div>
   );
