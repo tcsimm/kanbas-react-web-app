1d1ffb7a305fd7edff55f01205f0305cd85aecc7
tcsimm <widet77@icloud.com>
2024-07-30 09:03:03 -0400

    3.5.7

--- src/Labs/Lab5/WorkingWithArraysAsynchronously.tsx
+++ src/Labs/Lab5/WorkingWithArraysAsynchronously.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import * as client from "./client";
+import { FaTrash } from "react-icons/fa";
 
 export default function WorkingWithArraysAsynchronously() {
   const [todos, setTodos] = useState<any[]>([]);
@@ -7,6 +8,11 @@ export default function WorkingWithArraysAsynchronously() {
   const fetchTodos = async () => {
     const todos = await client.fetchTodos();
     setTodos(todos);
+  };
+
+  const removeTodo = async (todo: any) => {
+    const updatedTodos = await client.removeTodo(todo);
+    setTodos(updatedTodos);
   };
 
   useEffect(() => {
@@ -20,6 +26,11 @@ export default function WorkingWithArraysAsynchronously() {
       <ul className="list-group">
         {todos.map((todo) => (
           <li key={todo.id} className="list-group-item">
+            <FaTrash
+              onClick={() => removeTodo(todo)}
+              className="text-danger float-end mt-1"
+              id="wd-remove-todo"
+            />
             <input
               type="checkbox"
               className="form-check-input me-2"
