f839395942b2e73422e28b8e492e2f71ba87b992
tcsimm <widet77@icloud.com>
2024-07-29 21:44:01 -0400

    Env

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -1,8 +1,12 @@
+import EnvironmentVariables from "./EnvironmentVariables";
+
 export default function Lab5() {
-    return(
-        <div>
-            <h1>Lab 5</h1>
-            <a href="http://localhost:4000/lab5/welcome">Welcome</a>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Lab 5</h1>
+      <a href="http://localhost:4000/lab5/welcome">Welcome</a>
+      <hr />
+      <EnvironmentVariables />
+    </div>
+  );
 }
