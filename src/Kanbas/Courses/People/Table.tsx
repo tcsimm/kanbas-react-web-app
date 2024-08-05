a0604714f100ebbb426bd3d90aaa17f49568d139
tcsimm <widet77@icloud.com>
2024-08-05 11:48:09 -0400

    3.5

--- src/Kanbas/Courses/People/Table.tsx
+++ src/Kanbas/Courses/People/Table.tsx
@@ -1,20 +1,52 @@
 import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import * as client from "./client";
+
 export default function PeopleTable() {
+  const { cid } = useParams();
   const [users, setUsers] = useState<any[]>([]);
+  const [role, setRole] = useState("");
+
   const fetchUsers = async () => {
     const users = await client.findAllUsers();
     setUsers(users);
   };
+
+  const filterUsersByRole = async (role: string) => {
+    setRole(role);
+    if (role) {
+      const users = await client.findUsersByRole(role);
+      setUsers(users);
+    } else {
+      fetchUsers();
+    }
+  };
+
   useEffect(() => {
     fetchUsers();
   }, []);
+
   return (
     <div id="wd-people-table">
+      <select
+        value={role}
+        onChange={(e) => filterUsersByRole(e.target.value)}
+        className="form-select float-start w-25 wd-select-role"
+      >
+        <option value="">All Roles</option>
+        <option value="STUDENT">Students</option>
+        <option value="TA">Assistants</option>
+        <option value="FACULTY">Faculty</option>
+      </select>
       <table className="table table-striped">
         <thead>
           <tr>
-            <th>Name</th><th>Login ID</th><th>Section</th><th>Role</th><th>Last Activity</th><th>Total Activity</th>
+            <th>Name</th>
+            <th>Login ID</th>
+            <th>Section</th>
+            <th>Role</th>
+            <th>Last Activity</th>
+            <th>Total Activity</th>
           </tr>
         </thead>
         <tbody>
@@ -36,4 +68,3 @@ export default function PeopleTable() {
     </div>
   );
 }
-
--- src/Kanbas/Courses/People/client.ts
+++ src/Kanbas/Courses/People/client.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 export const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export const USERS_API = `${REMOTE_SERVER}/api/users`;
+
 export const findAllUsers = async () => {
   const response = await axios.get(USERS_API);
   return response.data;
 };
 
+export const findUsersByRole = async (role: string) => {
+  const response = await axios.get(`${USERS_API}?role=${role}`);
+  return response.data;
+};
